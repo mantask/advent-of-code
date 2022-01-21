6268408fc1f80ddab1a11c991e5b5db793b9e984
Mantas Kanaporis <mantas.kanaporis@gmail.com>
2022-01-22 01:46:51 +0200

    Day 12

---
+++ src/day12/day12a.kt
@@ -0,0 +1,28 @@
+package day12
+
+import java.io.File
+
+fun main() {
+
+    val graph = mutableMapOf<String, MutableList<String>>()
+
+    File("src/day12/full.in").forEachLine { line ->
+        val (node1, node2) = line.split('-')
+        graph.getOrPut(node1) { mutableListOf() }.add(node2)
+        graph.getOrPut(node2) { mutableListOf() }.add(node1)
+    }
+
+    fun String.isSmall() =
+        this[0].isLowerCase()
+
+    fun traverse(visited: Set<String>, last: String): Int =
+        graph[last]!!.sumOf { next ->
+            when {
+                next == "end" -> 1
+                next.isSmall() && visited.contains(next) -> 0
+                else -> traverse(visited + next, next)
+            }
+        }
+
+    println(traverse(setOf("start"), "start"))
+}
